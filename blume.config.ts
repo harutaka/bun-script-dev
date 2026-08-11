01fa2d5d9039004723caa6c0fa55bbe6d00e43bb
harutaka <10650294+harutaka@users.noreply.github.com>
2026-08-11 15:30:38 +0900

    add deployment base path

--- blume.config.ts
+++ blume.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   description: "Documentation powered by Blume.",
   feedback: false,
 
+  deployment: {
+    base: "/bun-script-dev",
+  },
+
   i18n: {
     defaultLocale: "ja",
     locales: [{ code: "ja", label: "日本語" }],
